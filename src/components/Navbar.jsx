6830c49be2d244e5dcbda8a57390ec79e1487f9b
Sanju Pal <158541486+sanju5683@users.noreply.github.com>
2025-05-07 21:31:10 +0530

    initial commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+// import React from 'react'
 
 function Navbar() {
     return (
-        <div className='flex bg-violet-500 font-semibold justify-between px-8 text-xl py-2'>
-            <div className="logo">iTask</div>
+        <div className='flex bg-violet-500 font-semibold justify-between px-8 text-4xl py-2'>
+            <div className="logo flex items-center gap-3"><img width={46} src="/public/task.png" alt="" />
+            <span>iTask </span>
+            </div>
             {/* <ul className='flex gap-5'>
                 <li>Home</li>
                 <li>Tasks</li>
