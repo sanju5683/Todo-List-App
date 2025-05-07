a7f16c72b60dfd6f52e951a2fdae0a9b069aa516
Sanju Pal <158541486+sanju5683@users.noreply.github.com>
2025-05-07 23:04:20 +0530

    initial commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@
 function Navbar() {
     return (
         <div className='flex bg-violet-500 font-semibold justify-between px-8 text-4xl py-2'>
-            <div className="logo flex items-center gap-3"><img width={46} src="/src/assets/task.png" alt="" />
+            <div className="logo flex items-center gap-3"><img width={46} src="/src/assets/note.png" alt="" />
             <span>iTask </span>
             </div>
             {/* <ul className='flex gap-5'>
